850a4cfae0c6e290e44a40bd8be554748d9543bf
A-r-y-a-n1 <rohelaaryan5@gmail.com>
2023-05-30 09:51:13 +0530

    ch2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,58 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     title: "Awesome App",
     home: HomePage(),
   ));
 }
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("awesome app"),
+        title: const Text("awesome app"),
       ),
-      body: Container(child: Center(child: Text("hi flutter"))),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Container(
+          color: Colors.black87,
+          height: MediaQuery.of(context).size.height,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.all(8),
+                color: Colors.red,
+                height: 100,
+                width: 100,
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                width: 100,
+                height: 100,
+                color: Colors.purple,
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                width: 100,
+                height: 100,
+                color: Colors.green,
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                width: 100,
+                height: 100,
+                color: Colors.teal,
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
